35a25f49232e3ba0c114ad6014c8e3e6a4fa6fca
Lauren Xie <lrnxie@gmail.com>
2024-06-10 11:51:47 -0400

    UI for home page

---
+++ app/_components/search-input.tsx
@@ -0,0 +1,9 @@
+import { Input } from '@/components/ui/input';
+
+export default function SearchInput() {
+  return (
+    <div className="mx-auto max-w-md">
+      <Input type="text" placeholder="Search" />
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,73 @@
+import Link from 'next/link';
+import SearchInput from './_components/search-input';
+
+const RECIPES = [
+  {
+    name: 'Kodak Potra 400',
+    tags: [
+      {
+        name: 'warm',
+        color: 'amber',
+      },
+    ],
+    preset: 'C1',
+  },
+  {
+    name: 'Fujicolor Superia 800',
+    tags: [
+      {
+        name: 'cool',
+        color: 'sky',
+      },
+    ],
+    preset: 'C2',
+  },
+  {
+    name: 'Cozy Chrome',
+    tags: [],
+    preset: '',
+  },
+];
+
 export default function Home() {
   return (
     <div>
-      <h1>hello fuji recipes</h1>
+      <SearchInput />
+
+      <ul className="mt-6 divide-y divide-zinc-100">
+        {RECIPES.map((recipe, index) => (
+          <li key={index} className="py-4">
+            <Link href="#" className="flex items-center space-x-4">
+              <div className="min-w-0 flex-1">
+                <p className="truncate text-sm font-medium text-zinc-900">
+                  {recipe.name}
+                </p>
+              </div>
+              <div className="space-x-2">
+                {recipe.tags.map((tag, index) => {
+                  const bg = `bg-${tag.color}-100`;
+                  const text = `text-${tag.color}-800`;
+
+                  return (
+                    <span
+                      key={index}
+                      className={`inline-flex items-center rounded-full ${bg} px-2.5 py-0.5 text-xs font-medium ${text}`}
+                    >
+                      {tag.name}
+                    </span>
+                  );
+                })}
+
+                {recipe.preset && (
+                  <span className="inline-flex items-center rounded bg-zinc-100 px-1.5 py-0.5 text-xs font-medium text-zinc-800">
+                    {recipe.preset}
+                  </span>
+                )}
+              </div>
+            </Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
